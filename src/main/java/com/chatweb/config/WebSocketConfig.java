ca5fbb1f3d3df8dadde8eb9b4f288a76f399a90e
hellowch <525691465@qq.com>
2020-12-14 14:11:33 +0800

    首先开始简单的文字聊天室

---
+++ src/main/java/com/chatweb/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.chatweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+
+    //注入ServerEndpointExporter bean对象，自动注册使用了@ServerEndpoint注解
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/com/chatweb/service/WebSocket.java
@@ -0,0 +1,11 @@
+package com.chatweb.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.server.ServerEndpoint;
+
+@Component
+@ServerEndpoint("/webSocket")
+public class WebSocket {
+
+}
